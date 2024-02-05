74946c0d0e7d231dcdf4b94c7644805a600b9a3f
Haritha Gunasekaran <harithaguna03@gmail.com>
2024-02-05 14:44:58 +0530

    email function added

---
+++ contact/contact.js
@@ -0,0 +1,36 @@
+var form=document.getElementById("contactForm");
+function submitForm()
+{
+  var fname=document.getElementById("firstName").value;
+  var lname=document.getElementById("lastName").value;
+  var email=document.getElementById("email").value;
+  var subject=document.getElementById("subject").value; 
+  var message=document.getElementById("message").value;
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "gunasekarang34539@gmail.com",
+    Password : "7A9DB4EE83B90B27D7C4ABD931A014DB0CF3",
+    To : 'gunasekarang34539@gmail.com',
+    From : "gunasekarang34539@gmail.com",
+    Subject : subject,
+    Body : message
+}).then(
+  message => {
+    if(message=="OK")
+  {
+    Swal.fire({
+        title: "Success!",
+        text: "Email Sent Successfully!",
+        icon: "success"
+      });
+  }
+  }
+);
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    submitForm();
+
+})
+
+}
